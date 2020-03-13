d6c14abd7de45ae12a9e1cbde5cc49f5c2204ba9
ZenRega38 <regarizz@gmail.com>
2020-03-13 10:37:28 +0700

    Adding Splash Screen and Navigation Bottom

--- app/src/main/java/com/regadeveloper/anudu/SplashScreen.java
+++ app/src/main/java/com/regadeveloper/anudu/SplashScreen.java
@@ -1,4 +1,27 @@
 package com.regadeveloper.anudu;
 
-public class SplashScreen {
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.annotation.Nullable;
+
+public class SplashScreen extends Activity {
+    private final int SPLASH_DISPLAY_LENGTH = 1000;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent splashIntent = new Intent(SplashScreen.this, SaungOptionsActivity.class);
+                SplashScreen.this.startActivity(splashIntent);
+                SplashScreen.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
 }
